aabaa59bb68e14e3020a4c8f5cf6f09b32a2f561
Abdulrahman <abdulrahman.saad2303@gmail.com>
2026-03-05 01:32:13 +0200

    refine: logo bg r

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -4,9 +4,9 @@ import { Stone } from "lucide-react";
 export function Logo({ inSidebar = false }: { inSidebar?: boolean }) {
   const logoStyle = inSidebar ? "w-5 h-5 p-0" : "h-6 w-6 p-0.5";
   return (
-    <div className="flex items-center gap-2 flex-col bg-foreground p-1.5 rounded-md">
+    <div className="flex items-center gap-2 flex-col bg-background p-1.5 rounded-md">
       <Stone
-        className={cn("h-6 w-6 stroke-background", logoStyle)}
+        className={cn("h-6 w-6 stroke-foreground", logoStyle)}
         strokeWidth={2}
       />
       {/* <span className="text-2xl font-bold tracking-tight">Bedrock</span> */}
